137ec7a978296f7450b643ca2fe4190f1dc2d87a
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2009-07-24 21:38:39 +0000

    Added CTIP stub
    git-svn-id: https://ccmc-software.googlecode.com/svn@850 acdf4c94-f73e-48c1-5845-2db45fb2849b

---
+++ Kameleon_Pro/src/ccmc/CTIP.cpp
@@ -0,0 +1,44 @@
+/*
+ * CTIP.cpp
+ *
+ *  Created on: Jul 24, 2009
+ *      Author: dberrios
+ */
+
+#include "CTIP.h"
+
+namespace ccmc
+{
+
+	CTIP::CTIP()
+	{
+		// TODO Auto-generated constructor stub
+
+	}
+
+	long CTIP::open(const std::string&)
+	{
+		return 0;
+	}
+
+	Interpolator* CTIP::createNewInterpolator()
+	{
+		return NULL;
+	}
+
+	void CTIP::initializeConversionFactorsToSI()
+	{
+
+	}
+
+	void CTIP::initializeSIUnits()
+	{
+
+	}
+
+	CTIP::~CTIP()
+	{
+		// TODO Auto-generated destructor stub
+	}
+
+}
