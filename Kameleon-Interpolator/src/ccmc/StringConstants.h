e81a7e03be1357ab3783fe37c9eeeda26a95f840
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2010-03-19 04:50:42 +0000

    Added c-style strings
    git-svn-id: https://ccmc-software.googlecode.com/svn@1014 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- Kameleon-Interpolator/src/ccmc/StringConstants.h
+++ Kameleon-Interpolator/src/ccmc/StringConstants.h
@@ -14,32 +14,66 @@ namespace ccmc
 	{
 		namespace variables
 		{
-			static std::string j_ = "j";
-			static std::string b_ = "b";
-			static std::string jx_ = "jx";
-			static std::string jy_ = "jy";
-			static std::string jz_ = "jz";
-			static std::string bx_ = "bx";
-			static std::string by_ = "by";
-			static std::string bz_ = "bz";
-			static std::string n_ = "n";
-			static std::string rho_ = "rho";
-			static std::string ux_ = "ux";
-			static std::string uy_ = "uy";
-			static std::string uz_ = "uz";
-			static std::string ex_ = "ex";
-			static std::string ey_ = "ey";
-			static std::string ez_ = "ez";
-			static std::string p_ = "p";
+			static const std::string j_ = "j";
+			static const char * j_c_ = "j";
+
+			static const std::string b_ = "b";
+			static const char * b_c_ = "b";
+
+			static const std::string jx_ = "jx";
+			static const char * jx_c_ = "jx";
+
+			static const std::string jy_ = "jy";
+			static const char * jy_c_ = "jy";
+
+			static const std::string jz_ = "jz";
+			static const char * jz_c_ = "jz";
+
+			static const std::string bx_ = "bx";
+			static const char * bx_c_ = "bx";
+
+			static const std::string by_ = "by";
+			static const char * by_c_ = "by";
+
+			static const std::string bz_ = "bz";
+			static const char * bz_c_ = "bz";
+
+			static const std::string n_ = "n";
+			static const char * n_c_ = "n";
+
+			static const std::string rho_ = "rho";
+			static const char * rho_c_ = "rho";
+
+			static const std::string ux_ = "ux";
+			static const char * ux_c_ = "ux";
+
+			static const std::string uy_ = "uy";
+			static const char * uy_c_ = "uy";
+
+			static const std::string uz_ = "uz";
+			static const char * uz_c_ = "uz";
+
+			static const std::string ex_ = "ex";
+			static const char * ex_c_ = "ex";
+
+			static const std::string ey_ = "ey";
+			static const char * ey_c_ = "ey";
+
+			static const std::string ez_ = "ez";
+			static const char * ez_c_ = "ez";
+
+			static const std::string p_ = "p";
+			static const char * p_c_ = "p";
+
 		}
 
 		namespace models
 		{
-			static std::string batsrus_ = "batsrus";
-			static std::string open_ggcm_ = "open_ggcm";
-			static std::string ucla_ggcm_ = "ucla_ggcm";
-			static std::string mas_ = "mas";
-			static std::string enlil_ = "enlil";
+			static const std::string batsrus_ = "batsrus";
+			static const std::string open_ggcm_ = "open_ggcm";
+			static const std::string ucla_ggcm_ = "ucla_ggcm";
+			static const std::string mas_ = "mas";
+			static const std::string enlil_ = "enlil";
 		}
 	}
 }
