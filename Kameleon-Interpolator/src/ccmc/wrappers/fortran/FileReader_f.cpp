175135b2fffafb9d727f7f50a88e33a482b38a77
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2010-03-22 22:47:54 +0000

    git-svn-id: https://ccmc-software.googlecode.com/svn@1047 acdf4c94-f73e-48c1-5845-2db45fb2849b

---
+++ Kameleon-Interpolator/src/ccmc/wrappers/fortran/FileReader_f.cpp
@@ -0,0 +1,95 @@
+/*
+ * FileReader_compatibility.h
+ *
+ *  Created on: Mar 16, 2010
+ *      Author: dberrios
+ */
+#include "FileReader_f.h"
+#include <ccmc/wrappers/c/FileReader_c.h>
+#include <ccmc/FileReader.h>
+#include <ccmc/Kameleon.h>
+#include <string>
+#include <string.h>
+#include <iostream>
+
+using namespace ccmc;
+
+
+
+
+void f_filereader_create_(int * id)
+{
+	FileReader_create(*id);
+}
+
+void f_filereader_open_(int * id, const char * filename, long * status)
+{
+	*status = FileReader_open(*id, filename);
+}
+
+void f_filereader_getvariable_(int * id, const char * variable, float * variableData)
+{
+	FileReader_getVariable(*id, variable, variableData);
+}
+
+void f_filereader_getvariablebyid_(int * id, long * variableID, float * variableData)
+{
+	FileReader_getVariableByID(*id, *variableID, variableData);
+}
+
+void f_filereader_getvariablesubrange_(int * id, const char * variable, long * startIndex, long * count, float * variableData)
+{
+	FileReader_getVariableSubRange(*id, variable, *startIndex, *count, variableData);
+}
+
+void f_filereader_getvariablebyidsubrange_(int * id, long * variableID, long * startIndex, long * count, float * variableData)
+{
+	FileReader_getVariableByIDSubRange(*id, *variableID, *startIndex, *count, variableData);
+}
+
+void f_filereader_getvariableid_(int * id, const char * variable, long * status)
+{
+	*status = FileReader_getVariableID(*id, variable);
+}
+
+void f_filereader_getvariableint(int id, const char * variable, int * variableData)
+{
+
+}
+
+void f_filereader_getnumberofglobalattributes(long * num)
+{
+
+}
+
+void f_filereader_getnumberofvariables(long * num)
+{
+
+}
+
+void f_filereader_getnumberofvariableattributes(long * num)
+{
+
+}
+
+void f_filereader_getnumberofrecords(int * id, const char * variable, long * num)
+{
+
+}
+
+void f_filereader_getnumberofrecordsbyid(int * id, long * variable_id, long * num)
+{
+
+}
+
+void f_filereader_close_(int * id, long * status)
+{
+	*status = FileReader_close(*id);
+}
+
+void f_filereader_delete_(int * id, long * status)
+{
+	*status = FileReader_delete(*id);
+}
+
+
---
+++ Kameleon-Interpolator/src/ccmc/wrappers/fortran/Kameleon_f.cpp
@@ -0,0 +1,13 @@
+/*
+ * Kameleon_compatibility.cpp
+ *
+ *  Created on: Jul 8, 2009
+ *      Author: dberrios
+ */
+#include "Kameleon_f.h"
+#include <ccmc/Kameleon.h>
+#include <string>
+#include <string.h>
+#include <iostream>
+
+
