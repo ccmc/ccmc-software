dfb913a9f4876c22d12c98a0a730511e32eb31f9
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2010-04-05 21:33:36 +0000

    git-svn-id: https://ccmc-software.googlecode.com/svn@321 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon_PLUS_interpolator/kameleon-plus/src/ccmc/FileReader.cpp
+++ kameleon_PLUS_interpolator/kameleon-plus/src/ccmc/FileReader.cpp
@@ -58,23 +58,20 @@ namespace ccmc
 
 			if (status == CDF_OK)
 			{
-std::cerr << "CDF_OK: attempting to initialize file" << std::endl;
+
 				//check if this is a valid Kameleon converted file
 
 				current_filename = filename;
 				this->initializeGlobalAttributes();
-				std::cerr << "After initializeGlobalAttributes" << std::endl;
 				this->initializeVariableAttributes();
-				std::cerr << "After initializeVariableAttributes" << std::endl;
 				this->initializeVariableIDs();
-				std::cerr << "After initializeVariableIDs" << std::endl;
 				this->initializeVariableNames();
-				std::cerr << "AfterinitializeVariableNames" << std::endl;
 			}
 
 		}
 		//cout << "current_file_id: " << current_file_id << endl;
 		//cout << "testing open in FileReader class" << endl;
+
 		return status;
 	}
 
