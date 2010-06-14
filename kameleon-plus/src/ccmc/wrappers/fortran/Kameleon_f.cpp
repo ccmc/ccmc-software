984cb668dd06a96aaa5116d19956db72137223f4
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2010-06-14 17:20:04 +0000

    git-svn-id: https://ccmc-software.googlecode.com/svn@1130 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/wrappers/fortran/Kameleon_f.cpp
+++ kameleon-plus/src/ccmc/wrappers/fortran/Kameleon_f.cpp
@@ -11,23 +11,60 @@
 #include <string.h>
 #include <iostream>
 
+using namespace ccmc;
 
 void f_kameleon_create_(int * id)
 {
+//	std::cout << "calling C create function" << std::endl;
 	Kameleon_create(*id);
+//	std::cout << "after C create function" << std::endl;
 }
 
 void f_kameleon_open_(int * id, const char * filename, long * status)
 {
-	*status = Kameleon_open(*id, filename);
+	std::string filename_str = filename;
+	//get string length
+	int length = filename_str.size();
+	char filename_corrected[length+1];
+	strcpy(filename_corrected, filename_str.c_str());
+	//std::cout << "string length: " << length << std::endl;
+	for (int i = length; i> 1; i--)
+	{
+		if (filename_corrected[i-1] != ' ')
+		{
+	//		std::cout << "add the character at index: " << i << std::endl;
+			filename_corrected[i] = '\0';
+			break;
+		}
+
+	}
+	*status = Kameleon_open(*id, filename_corrected);
 }
 //extern _C_ void f_kameleon_get_model_name(int id, char * model_name);
 //extern _C_ float Kameleon_interpolate(int id, const char * variable, const float * c0,
 //		const float * c1, const float *c2, float * dc0, float * dc1, float * dc2);
 //extern _C_ void f_kameleon_load_variable(int *id, const char * variable);
+
+/**
+ * This creates a copy of the string to make it compatible with C. The better way would be to
+ * pass the string to this function with a null terminal at the correct position. Not sure how
+ * to do that in fortran
+ */
 void f_kameleon_load_vector_variable_(int *id, const char * variable)
 {
-	Kameleon_load_vector_variable(*id, variable);
+	std::string variable_str = variable;
+	int length = variable_str.size();
+	char variable_corrected[length+1];
+	strcpy(variable_corrected, variable_str.c_str());
+	for (int i = length; i>1; i--)
+	{
+		if (variable_corrected[i-1] != ' ')
+		{
+			variable_corrected[i] = '\0';
+			break;
+		}
+	}
+	Kameleon_load_vector_variable(*id, variable_corrected);
 }
 
 //extern _C_ void Kameleon_get_global_attribute_string(int id, const char * gAttribute, char * destbuffer);
