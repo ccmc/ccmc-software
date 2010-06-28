fcc39b583c6cca09976f8016c1040ec5999a1efa
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2010-06-28 19:19:38 +0000

    git-svn-id: https://ccmc-software.googlecode.com/svn@357 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon_PLUS_interpolator/kameleon-plus/src/example/fortran/Tracer_test_f.f
+++ kameleon_PLUS_interpolator/kameleon-plus/src/example/fortran/Tracer_test_f.f
@@ -4,14 +4,17 @@ c     Five functions to create a Kameleon Object, open the cdf file, load the va
  
       external f_kameleon_create, f_kameleon_open, f_kameleon_delete
       external f_kameleon_load_vector_variable, f_kameleon_close
+      external f_kameleon_create_c_string
       
 c     Three Tracer functions
       external f_tracer_create, f_tracer_bidirectionaltrace, f_tracer_delete
  
 c     Variables to be used for interpolation and data extraction
 
-      character*300 cdf_file_path 
+      character*300 cdf_file_path
+      character*301 cdf_file_path_c 
       character*50 variable
+      character*50 variable_c
 c      real*8 x,y,z
 c      real*8 interpolated_value
       integer status,kid,tid,s_length,actual_steps
@@ -26,13 +29,14 @@ c     --- set your actual path name here ---
       call getarg(1, cdf_file_path)
       call getarg(2, variable)
 c      cdf_file_path='/Users/dberrios/Desktop/test.cdf'//CHAR(0)
+c      trim(filename)
       s_length=len(cdf_file_path)
       write(*,*) 's_length for cdf_file_path: ',s_length
-      cdf_file_path(s_length:s_length)=CHAR(0)
+c      cdf_file_path(s_length:s_length)=CHAR(0)
       
-      s_length=len(variable)
-      write(*,*) 's_length for variable: ',s_length
-      variable(s_length:s_length)=CHAR(0)
+c      s_length=len(variable)
+c      write(*,*) 's_length for variable: ',s_length
+c      variable(s_length:s_length)=CHAR(0)
       
       write(*,*) 'Processing file: ', cdf_file_path
 c      write(*,*) 'Note:  The above referenced file name is 
@@ -47,10 +51,13 @@ c     Open the cdf file
       tid=0
       status=0
       actual_steps=0
+      call f_kameleon_create_c_string(cdf_file_path, cdf_file_path_c)
+      call f_kameleon_create_c_string(variable, variable_c)
       call f_kameleon_create(kid)
-      call f_Kameleon_open(kid,cdf_file_path,status)
-      call f_Kameleon_load_vector_variable(kid,variable)
+      call f_Kameleon_open(kid,cdf_file_path_c,status)
+      call f_Kameleon_load_vector_variable(kid,variable_c)
       call f_Tracer_create(tid,kid)
+      
 c     make sure the step size used is appropriate and the target arrays have 2 times
 c     the step_max so the arrays have enough for two directions.
       call f_Tracer_bidirectionaltrace(tid,variable,-10.,0.,0.,500,.2, 
