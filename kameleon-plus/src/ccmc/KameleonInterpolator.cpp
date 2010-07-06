a389301031719f6b9720a3be55bb7e38b34a1473
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2010-07-06 21:30:04 +0000

    Added comments
    git-svn-id: https://ccmc-software.googlecode.com/svn@1172 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/KameleonInterpolator.cpp
+++ kameleon-plus/src/ccmc/KameleonInterpolator.cpp
@@ -10,6 +10,9 @@
 
 namespace ccmc
 {
+	/**
+	 * @param model
+	 */
 	KameleonInterpolator::KameleonInterpolator(Model * model)
 	{
 
@@ -21,6 +24,13 @@ namespace ccmc
 
 	}
 
+	/**
+	 * @param variable
+	 * @param c0
+	 * @param c1
+	 * @param c2
+	 * @return
+	 */
 	float KameleonInterpolator::interpolate(const std::string& variable, const float& c0, const float& c1,
 			const float& c2)
 	{
@@ -30,6 +40,16 @@ namespace ccmc
 		return interpolate(variable, c0, c1, c2, dc0, dc1, dc2);
 	}
 
+	/**
+	 * @param variable
+	 * @param c0
+	 * @param c1
+	 * @param c2
+	 * @param dc0
+	 * @param dc1
+	 * @param dc2
+	 * @param return
+	 */
 	float KameleonInterpolator::interpolate(const std::string& variable, const float& c0, const float& c1,
 			const float& c2, float& dc0, float& dc1, float& dc2)
 	{
@@ -65,6 +85,12 @@ namespace ccmc
 
 	/**
 	 * Does nothing.  A variable_id won't work well, since derived variables can be requested, which do not exist in the data.
+	 *
+	 * @param variable_id
+	 * @param c0
+	 * @param c1
+	 * @param c2
+	 * @return
 	 */
 	float KameleonInterpolator::interpolate(long variable_id, const float& c0, const float& c1, const float& c2)
 	{
@@ -108,6 +134,9 @@ namespace ccmc
 	 * For some vector variables, the conversion factors need to be specified for the magnitude
 	 * calculations as well, since some of the magnitude calculations are independent of the vector
 	 * component calculations (to increase performance).
+	 */
+	/**
+	 *
 	 */
 	void KameleonInterpolator::initializeConversionFactorsToVis()
 	{
@@ -218,6 +247,13 @@ namespace ccmc
 		}
 	}
 
+	/**
+	 * @param variable
+	 * @param c0
+	 * @param c1
+	 * @param c2
+	 * @return
+	 */
 	std::vector<float>* KameleonInterpolator::interpolateSimple(std::vector<std::string>& variables, const float& c0, const float& c1, const float& c2)
 	{
 		std::vector<float>* interpolatedData = new std::vector<float>();
@@ -228,6 +264,13 @@ namespace ccmc
 		return interpolatedData;
 	}
 
+	/**
+	 * @param variables
+	 * @param c0
+	 * @param c1
+	 * @param c2
+	 * @return
+	 */
 	std::vector<float>* KameleonInterpolator::interpolateSimple(std::vector<long>& variables, const float& c0, const float& c1, const float& c2)
 	{
 		std::vector<float>* interpolatedData = new std::vector<float>();
@@ -238,6 +281,9 @@ namespace ccmc
 		return interpolatedData;
 	}
 
+	/**
+	 *
+	 */
 	KameleonInterpolator::~KameleonInterpolator()
 	{
 		// TODO Auto-generated destructor stub
