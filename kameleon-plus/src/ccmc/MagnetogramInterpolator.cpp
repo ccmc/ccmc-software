fea8d389518f6361db188b3f8d6c76eeab05732a
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2010-07-06 21:30:14 +0000

    Added comments
    git-svn-id: https://ccmc-software.googlecode.com/svn@1173 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/MagnetogramInterpolator.cpp
+++ kameleon-plus/src/ccmc/MagnetogramInterpolator.cpp
@@ -9,33 +9,73 @@
 
 namespace ccmc
 {
+	/**
+	 * @param model
+	 */
 	MagnetogramInterpolator::MagnetogramInterpolator(Model * model)
 	{
 		// TODO Auto-generated constructor stub
 
 	}
 
+	/**
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @return
+	 */
 	float MagnetogramInterpolator::interpolate(const std::string&, const float&, const float&, const float&)
 	{
 		return 0.f;
 	}
 
+	/**
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @return
+	 */
 	float MagnetogramInterpolator::interpolate(const std::string&, const float&, const float&, const float&, float&, float&,
 			float&)
 	{
 		return 0.f;
 	}
 
+	/**
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @return
+	 */
 	float MagnetogramInterpolator::interpolate(long int, const float&, const float&, const float&)
 	{
 		return 0.f;
 	}
 
+	/**
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @return
+	 */
 	float MagnetogramInterpolator::interpolate(long int, const float&, const float&, const float&, float&, float&, float&)
 	{
 		return 0.f;
 	}
 
+	/**
+	 *
+	 */
 	MagnetogramInterpolator::~MagnetogramInterpolator()
 	{
 		// TODO Auto-generated destructor stub
