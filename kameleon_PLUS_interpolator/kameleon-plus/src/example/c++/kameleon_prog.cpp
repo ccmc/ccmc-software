6603da545c7639ebaaab35a96e0a27446fbbc549
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2010-11-04 21:54:21 +0000

    git-svn-id: https://ccmc-software.googlecode.com/svn@425 acdf4c94-f73e-48c1-5845-2db45fb2849b

---
+++ kameleon_PLUS_interpolator/kameleon-plus/src/example/c++/kameleon_prog.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <ctime>
+#include <ccmc/Kameleon.h>
+#include <ccmc/FileReader.h>
+#include <boost/lexical_cast.hpp>
+
+int main (int argc, char * argv[])
+{
+
+	Kameleon kameleon;
+	std::string filename;
+	std::string variable;
+	int iterations = 10;
+	float c0,c1,c2;
+	if (argc != 6)
+	{
+		cout << "kameleon <filename> variable c0 c1 c2\n";
+		cout << "\tOpenGGCM, BATSRUS: x y z\n";
+		cout << "\tENLIL, MAS: r phi(latitude) theta(longitude)" << endl;
+		exit(1);
+	}
+	filename = argv[1];
+	variable = argv[2];
+	c0 = boost::lexical_cast<float>(argv[3]);
+	c1 = boost::lexical_cast<float>(argv[4]);
+	c2 = boost::lexical_cast<float>(argv[5]);
+	long status = kameleon.open(filename);
+	if (status == FileReader::OK)
+	{
+		std::cout << "Opened file: " << filename << " with status: " << status << std::endl;
+		std::string rho_ = "rho";
+		std::string n_ = "n";
+		std::string pram_ = "pram";
+		std::cout << "Model name: " << kameleon.getGlobalAttribute("model_name").getAttributeString() << std::endl;
+		std::cout << "Loading variables" << std::endl;
+		bool successLoading = kameleon.loadVariable(variable);
+		bool success = kameleon.doesVariableExist(variable);
+		std::cout << "Creating new interpolator" << std::endl;
+		Interpolator * interpolator = kameleon.createNewInterpolator();
+		clock_t start, finish;
+
+		start = clock();
+
+		float conversion = 1.0f;
+		//float conversion = interpolator->getConversionFactorToVis(variable);
+		float value;
+		//std::string bx_ = "br";
+		float convertedValue;
+	//	ProfilerStart("derived.prof");
+		//Interpolations.  The i%2 ensures the worst case scenario (previous block differe88nt than current block
+		for (int i = 0; i < 1000; i++)
+		{
+			if (i % 100 == 0)
+			{
+				//std::cout << "i: " << i << std::endl;
+			}
+			if (success)
+			{
+				value = interpolator->interpolate(variable, c0,c1,c2 );
+			} else
+				value = kameleon.getMissingValue();
+			convertedValue = value * conversion;
+
+		}
+	//	ProfilerStop();
+		std::cout << "conversionFactor: " << conversion << std::endl;
+		std::cout << "value: " << value << " " << kameleon.getNativeUnit(variable) << " convertedValue: " <<
+			convertedValue << " " << kameleon.getVisUnit(variable) << std::endl;
+
+
+		finish = clock();
+		float elapsed_time = ((double) finish - (double) start) / CLOCKS_PER_SEC;
+
+		std::cout << "test for disk access: " << interpolator->interpolate(variable, -10.f, -2.f, -0.f) << std::endl;
+		delete interpolator;
+
+		std::cout << "Elapsed time for interpolation: " << elapsed_time << std::endl;
+
+
+
+
+		kameleon.close();
+	}
+	return 0;
+}
