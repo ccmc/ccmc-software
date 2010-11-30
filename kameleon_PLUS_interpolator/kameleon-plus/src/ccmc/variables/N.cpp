b98c8f6a62896037d725a9f4a19f7b8a9851dbd9
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2010-11-30 19:05:19 +0000

    git-svn-id: https://ccmc-software.googlecode.com/svn@460 acdf4c94-f73e-48c1-5845-2db45fb2849b

---
+++ kameleon_PLUS_interpolator/kameleon-plus/src/ccmc/variables/N.cpp
@@ -0,0 +1,19 @@
+/*
+ * N.cpp
+ *
+ *  Created on: Nov 24, 2010
+ *      Author: David Berrios
+ */
+
+#include "N.h"
+
+N::N()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+N::~N()
+{
+	// TODO Auto-generated destructor stub
+}
