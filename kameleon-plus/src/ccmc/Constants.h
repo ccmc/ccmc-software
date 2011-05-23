ff845f3db237156d5e8f96b6bea19d15e8b7538d
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2011-05-23 20:38:25 +0000

    Renamed constants
    git-svn-id: https://ccmc-software.googlecode.com/svn@1336 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/Constants.h
+++ kameleon-plus/src/ccmc/Constants.h
@@ -23,8 +23,8 @@ namespace ccmc
 		static float Pi = 3.14159265;
 		static float AU_in_meters = 1.49598e11;
 		static float Meters_to_AU = 6.68458134e-12;
-		static float Radian_in_degrees = 57.2957795;
-		static float Degree_in_radians = 0.0174532925;
+		static float RadiansToDegrees = 57.2957795;
+		static float DegreesToRadians = 0.0174532925;
 
 	}
 
