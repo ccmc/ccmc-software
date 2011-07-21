4cefca6ee5a3efc4581c99e4dab56faff4301748
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2011-07-21 00:12:45 +0000

    init commit
    git-svn-id: https://ccmc-software.googlecode.com/svn@1390 acdf4c94-f73e-48c1-5845-2db45fb2849b

---
+++ kameleon-plus/src/example/c++/adapt3d_test.cpp
@@ -0,0 +1,46 @@
+/*
+ * adapt3d_test.cpp
+ *
+ *  Created on: Sep 23, 2009
+ *      Author: dberrios
+ */
+
+#include <boost/lexical_cast.hpp>
+#include <iostream>
+#include <ccmc/Adapt3D.h>
+#include <ccmc/Interpolator.h>
+#include <string>
+
+using namespace std;
+
+/**
+ * adapt3d <inputfile> variable x y z
+ */
+int main (int argc, char * argv[])
+{
+	if (argc != 6)
+	{
+		cout << "adapt3d <inputfile> variable x y z" << endl;
+		exit(1);
+	}
+
+	string filename = argv[1];
+	string variable = argv[2];
+	float x = boost::lexical_cast<float>(argv[3]);
+	float y = boost::lexical_cast<float>(argv[4]);
+	float z = boost::lexical_cast<float>(argv[5]);
+	ccmc::Adapt3D adapt3d;
+	adapt3d.open(filename);
+	adapt3d.loadVariable(variable);
+
+	ccmc::Interpolator * interpolator = adapt3d.createNewInterpolator();
+	float value = interpolator->interpolate(variable, x, y, z);
+	float density = interpolator->interpolate("rho", x, y, z);
+	delete interpolator;
+
+	string units = adapt3d.getNativeUnit(variable);
+
+	cout << "interpolated value: " << value/density << " " << units << endl;
+	adapt3d.close();
+
+}
