047d585a0782da0edd0ecab5bdc800d40edecab2
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2011-09-14 19:47:51 +0000

    update
    git-svn-id: https://ccmc-software.googlecode.com/svn@612 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon_PLUS_interpolator/kameleon-plus/src/example/c++/kameleon_prog.cpp
+++ kameleon_PLUS_interpolator/kameleon-plus/src/example/c++/kameleon_prog.cpp
@@ -19,9 +19,9 @@ int main (int argc, char * argv[])
 	float c0,c1,c2;
 	if (argc != 6)
 	{
-		cout << "kameleon <filename> variable c0 c1 c2\n";
-		cout << "\tOpenGGCM, BATSRUS: x y z\n";
-		cout << "\tENLIL, MAS: r theta(latitude) phi(longitude)" << endl;
+		std::cout << "kameleon <filename> variable c0 c1 c2\n";
+		std::cout << "\tAdapt3D, OpenGGCM, BATSRUS: x y z\n";
+		std::cout << "\tENLIL, MAS: r theta(latitude) phi(longitude)" << std::endl;
 		exit(1);
 	}
 	filename = argv[1];
@@ -30,18 +30,20 @@ int main (int argc, char * argv[])
 	c1 = boost::lexical_cast<float>(argv[4]);
 	c2 = boost::lexical_cast<float>(argv[5]);
 	long status = kameleon.open(filename);
+	std::cout << "Opened file: " << filename << " with status: " << status << std::endl;
+	std::cout << "FileReader::OK = " << FileReader::OK << std::endl;
 	if (status == FileReader::OK)
 	{
-		std::cout << "Opened file: " << filename << " with status: " << status << std::endl;
 		std::string rho_ = "rho";
 		std::string n_ = "n";
 		std::string pram_ = "pram";
+		std::cout << "Getting model name" << std::endl;
 		std::cout << "Model name: " << kameleon.getGlobalAttribute("model_name").getAttributeString() << std::endl;
 		std::cout << "Loading variables" << std::endl;
 		bool successLoading = kameleon.loadVariable(variable);
 		std::cout << "Variable: " << variable << " load status: " << successLoading << std::endl;
-		bool success = kameleon.doesVariableExist(variable);
-		std::cout << "Does " << variable << " exist? " << success << std::endl;
+		//bool success = kameleon.doesVariableExist(variable);
+		//std::cout << "Does " << variable << " exist? " << success << std::endl;
 		//std::cout << "Creating new interpolator" << std::endl;
 		Interpolator * interpolator = kameleon.createNewInterpolator();
 		clock_t start, finish;
@@ -57,13 +59,8 @@ int main (int argc, char * argv[])
 		float x,y,z;
 		srand((unsigned)time(0));
 		int range = 10;
-		if (success)
-		{
-			value = interpolator->interpolate(variable, c0,c1,c2 );
-		} else
-			value = kameleon.getMissingValue();
 
-		std::cout << "value before random interpolations: " << value << std::endl;
+		/*std::cout << "value before random interpolations: " << value << std::endl;
 		for (int i = 0; i < 1; i++)
 		{
 			for (int j = 0; j < 1; j++)
@@ -88,23 +85,30 @@ int main (int argc, char * argv[])
 					//std::cerr << x << "," << y << "," << z << ": " << value << std::endl;
 				}
 			}
-		}
-		if (success)
+		}*/
+		if (successLoading)
+		{
+			value = interpolator->interpolate(variable, c0,c1,c2 );
+		} else
+			value = kameleon.getMissingValue();
+
+		/*if (successLoading)
 		{
 			value = interpolator->interpolate(variable, c0,c1,c2 );
 		} else
 			value = kameleon.getMissingValue();
 		convertedValue = value * conversion;
 
-		std::cout << "value after random interpolations: " << value << std::endl;
-		if (success)
+		//std::cout << "value after random interpolations: " << value << std::endl;
+		if (successLoading)
 		{
 			value = interpolator->interpolate(variable, c0,c1,c2 );
 		} else
 			value = kameleon.getMissingValue();
 		convertedValue = value * conversion;
+*/
 
-		std::cout << "value after random interpolations: " << value << std::endl;
+		std::cout << "value: " << value << std::endl;
 		//std::cout << "conversionFactor: " << conversion << std::endl;
 		//std::cout << "value: " << value << " " << kameleon.getNativeUnit(variable) << " convertedValue: " <<
 		//	convertedValue << " " << kameleon.getVisUnit(variable) << std::endl;
