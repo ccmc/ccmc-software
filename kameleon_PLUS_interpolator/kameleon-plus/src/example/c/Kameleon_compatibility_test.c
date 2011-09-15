229adb0d914923c7c053510b9c924fac48bfc249
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2011-09-15 19:34:11 +0000

    git-svn-id: https://ccmc-software.googlecode.com/svn@630 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon_PLUS_interpolator/kameleon-plus/src/example/c/Kameleon_compatibility_test.c
+++ kameleon_PLUS_interpolator/kameleon-plus/src/example/c/Kameleon_compatibility_test.c
@@ -5,7 +5,7 @@
  *      Author: dberrios
  */
 
-#include <ccmc/wrappers/c/Kameleon_c.h>
+#include <ccmc/Kameleon.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -16,46 +16,11 @@ int main( int argc, char * argv[])
 		printf("Must enter 1 filename1\n");
 		return 1;
 	}
-	int k0 = Kameleon_create();
-	int k1 = Kameleon_create();
 
-
-	Kameleon_open(k0,argv[1]);
-	Kameleon_open(k1,argv[1]);
-
-	//get model names
-	char modelName1[512];
-	char modelName2[512];
-	Kameleon_get_model_name(k1, modelName1);
-	Kameleon_get_model_name(k0, modelName2);
-
-	printf("model1: %s\n",modelName1);
-	printf("model2: %s\n",modelName2);
-
-	float c0,c1,c2,dc0,dc1,dc2;
-	c0 = -10.f;
-	c1 = -2.f;
-	c2 = 0.f;
-	Kameleon_load_variable(k0,"bx");
-	Kameleon_load_variable(k1,"bx");
-
-	int i0 = Interpolator_create(k0);
-	int i1 = Interpolator_create(k1);
-	float value1 = Kameleon_interpolate(i0,"bx",&c0,&c1,&c2,&dc0,&dc1,&dc2);
-	float value2 = Kameleon_interpolate(i1,"bx",&c0,&c1,&c2,&dc0,&dc1,&dc2);
-	value1 = Kameleon_interpolate(i0,"bx",&c0,&c1,&c2,&dc0,&dc1,&dc2);
-	value2 = Kameleon_interpolate(i1,"bx",&c0,&c1,&c2,&dc0,&dc1,&dc2);
-	printf("value1: %f\n",value1);
-	printf("value2: %f\n",value2);
-
-	int status = Interpolator_delete(i0);
-	printf("idelete: %d\n", status);
-	status = Interpolator_delete(i1);
-	printf("idelete: %d\n", status);
-	status = Kameleon_delete(k0);
-	printf("kdelete: %d\n", status);
-	status = Kameleon_delete(k1);
-	printf("kdelete: %d\n", status);
-
+	printf("creating kameleon object\n");
+	Kameleon * kameleon = Kameleon_new();
+	printf("created kameleon object\n");
+	Kameleon_delete(kameleon);
+	printf("deleted kameleon\n");
 	return 0;
 }
