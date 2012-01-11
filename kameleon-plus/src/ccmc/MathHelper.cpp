1137b004077c5f171829865b0513bdc4037737c3
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2012-01-11 19:07:51 +0000

    Adapt3D helpers
    git-svn-id: https://ccmc-software.googlecode.com/svn@1446 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/MathHelper.cpp
+++ kameleon-plus/src/ccmc/MathHelper.cpp
@@ -7,6 +7,7 @@
 
 #include "MathHelper.h"
 #include <limits>
+#include <cmath>
 #include <algorithm>
 
 namespace ccmc
@@ -207,6 +208,63 @@ namespace ccmc
 					 max=array[i];
 		   }
 	   return max;
+	}
+
+	void Math::convert_xyz_to_rthetaphi(const float& x, const float& y, const float& z,
+			float * r, float * t, float * p)
+	{
+	/*
+	 Converts 3D cartesian coords (x,y,z) into spherical coords (r,theta,phi).
+	*/
+
+	      float  piov2,r2,zr,yx;
+
+	      piov2 = std::acos(0.);
+
+	      r2 = x*x+y*y+z*z;
+	      if(r2 > 0.) {
+	        *r  = std::sqrt(r2);
+	        zr=z/ *r;
+	        *t = acos(zr);
+	        if(x > 0.) {
+	          yx=y/x;
+	          *p = std::atan(yx);
+	        } else if(x < 0.) {
+	          yx=y/std::fabs(x);
+	          *p = 2.*piov2 - atan(yx);
+	        } else {
+	          *p = piov2*sign(y);
+	        }
+	      } else {
+	        *r = 0.;
+	        *t = 0.;
+	        *p = 0.;
+	      }
+	      if(*p < 0.) *p=*p+4.*piov2;               /* pmn testing */
+	}
+
+	void Math::convert_rthetaphi_to_xyz(const float& r, const float& t, const float& p,
+			float * x, float * y, float * z)
+	{
+	/*
+	  Converts spherical coords (r,theta,phi) into 3D cartesian coords (x,y,z)
+	*/
+
+	      float  sint,cost,sinp,cosp;
+
+	      sint = std::sin(t);
+	      cost = std::cos(t);
+	      sinp = std::sin(p);
+	      cosp = std::cos(p);
+
+	      *x = r*sint*cosp;
+	      *y = r*sint*sinp;
+	      *z = r*cost;
+	}
+
+	int Math::sign(int v)
+	{
+	   return v > 0 ? 1 : (v < 0 ? -1 : 0);
 	}
 
 }
