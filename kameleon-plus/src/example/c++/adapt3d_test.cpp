72f8cc942273e693682ffc3431373fdc33a0fa63
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2012-01-19 13:15:50 +0000

    git-svn-id: https://ccmc-software.googlecode.com/svn@1456 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/example/c++/adapt3d_test.cpp
+++ kameleon-plus/src/example/c++/adapt3d_test.cpp
@@ -12,8 +12,6 @@
 #include <string>
 #include <ctime>
 
-using namespace std;
-
 /**
  * adapt3d <inputfile> variable x y z
  */
@@ -21,38 +19,40 @@ int main (int argc, char * argv[])
 {
 	if (argc != 6)
 	{
-		cout << "adapt3d <inputfile> variable x y z" << endl;
+		std::cout << "adapt3d <inputfile> variable x y z" << std::endl;
 		exit(1);
 	}
+	ccmc::Adapt3D adapt3d;
+	//adapt3d = new ccmc::Adapt3D();
 
-	string filename = argv[1];
-	string variable = argv[2];
+	std::string filename = argv[1];
+	std::string variable = argv[2];
 	float x = boost::lexical_cast<float>(argv[3]);
 	float y = boost::lexical_cast<float>(argv[4]);
 	float z = boost::lexical_cast<float>(argv[5]);
-	ccmc::Adapt3D adapt3d;
+
 	adapt3d.open(filename);
 	adapt3d.loadVariable(variable);
 
 	clock_t begin = clock();
 	ccmc::Interpolator * interpolator = adapt3d.createNewInterpolator();
-	/*int upper = 5;
-	float delta = 10.f/(float)(upper -1);
-	for (int i = 0; i < upper; i++)
-	{
-		for (int j = 0; j < upper; j++)
-		{
-			for (int k = 0; k < upper; k++)
-			{
-				float x = -5.f+delta*(float)i;
-				float y = -5.f+delta*(float)j;
-				float z = -5.f+delta*(float)k;
-				//std::cout << "x: " << x << " y: " << y << " z: " << z << std::endl;
-				float value = interpolator->interpolate(variable, x,y,z);
-
-			}
-		}
-	}*/
+//	int upper = 5;
+//	float delta = 10.f/(float)(upper -1);
+//	for (int i = 0; i < upper; i++)
+//	{
+//		for (int j = 0; j < upper; j++)
+//		{
+//			for (int k = 0; k < upper; k++)
+//			{
+//				float x = -5.f+delta*(float)i;
+//				float y = -5.f+delta*(float)j;
+//				float z = -5.f+delta*(float)k;
+//				//std::cout << "x: " << x << " y: " << y << " z: " << z << std::endl;
+//				float value = interpolator->interpolate(variable, x,y,z);
+//
+//			}
+//		}
+//	}
 	clock_t end = clock();
 	float final = (end-begin)/CLOCKS_PER_SEC;
 	std::cout << "elapsed time: " << final << " seconds" << std::endl;
@@ -60,9 +60,9 @@ int main (int argc, char * argv[])
 	//float density = interpolator->interpolate("rho", x, y, z);
 	delete interpolator;
 
-	string units = adapt3d.getNativeUnit(variable);
+	std::string units = adapt3d.getNativeUnit(variable);
 
-	cout << "interpolated value: " << value << " " << units << endl;
+	std::cout << "interpolated value: " << value << " " << units << std::endl;
 	adapt3d.close();
 	return 0;
 
