17fdf94267f48fdf08bc069d76aed478e9cf03ba
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2012-06-07 19:25:26 +0000

    Added SWMFIonoInterpolator
    git-svn-id: https://ccmc-software.googlecode.com/svn@1475 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/SWMFIonoInterpolator.cpp
+++ kameleon-plus/src/ccmc/SWMFIonoInterpolator.cpp
@@ -1,106 +1,180 @@
-//
-//  SWMFInterpolator.cpp
-//  
-//
-//  Created by Nitesh Donti on 1/11/12.
-//  Copyright 2012 Cornell University. All rights reserved.
-//
+/*
+ * SWMFIonoInterpolator.cpp
+ *
+ *  Created on: May 25, 2012
+ *      Author: David Berrios
+ */
 
 #include "SWMFIonoInterpolator.h"
 #include "SWMFIono.h"
 #include "StringConstants.h"
 #include "Constants.h"
 #include "Utils.h"
+#include <math.h>
 
 namespace ccmc {
 
-SWMFInterpolator::SWMFInterpolator(Model * model){
+	SWMFIonoInterpolator::SWMFIonoInterpolator(Model * model)
+	{
 
-	this->modelReader = model;
-	this->setMissingValue(this->modelReader->getMissingValue());
+		this->modelReader = model;
+		modelReader->setMissingValue(this->modelReader->getMissingValue());
 
-	r_string = ccmc::strings::variables::r_;
-	th_string = ccmc::strings::variables::theta_;
-	phi_string = ccmc::strings::variables::phi_;
+		//latitude
+		th_string = ccmc::strings::variables::theta_;
 
-	r_data = modelReader->getVariableData(r_string);
-	th_data = modelReader->getVariableData(th_string);
-	phi_data = modelReader->getVariableData(phi_string);
+		//longitude
+		psi_string = ccmc::strings::variables::psi_;
+		psi_size = modelReader->getGlobalAttribute("psi_size").getAttributeInt();
+		theta_size = modelReader->getGlobalAttribute("theta_size").getAttributeInt();
 
-	previous_r = missingValue;
-	previous_th = missingValue;
-	previous_phi = missingValue;
+		th_data = modelReader->getVariableFromMap(th_string);
+		psi_data = modelReader->getVariableFromMap(psi_string);
+		float missingValue = this->modelReader->getMissingValue();
+		previous_th = missingValue;
+		previous_psi = missingValue;
+		//previous_phi = missingValue;
 
-}
+	}
 
-float SWMFInterpolator::interpolate(const std::string& variable, const float& r, const float& lat, const float& lon){
+	float SWMFIonoInterpolator::interpolate(const std::string& variable, const float& r, const float& lat, const float& lon){
 
-	float dr, dlat, dlon;
-	long variable_id = modelReader->getVariableID(variable);
+		float dr, dlat, dlon;
+		long variable_id = modelReader->getVariableID(variable);
 
-	return interpolate(variable_id, r, lat, lon, dr, dlat, dlon);
-}
+		return interpolate(variable_id, r, lat, lon, dr, dlat, dlon);
+	}
 
-float SWMFInterpolator::interpolate(const std::string& variable, const float& r, const float& lat, const float& lon, float& dr, float& dlat, float& dlon){
+	float SWMFIonoInterpolator::interpolate(const std::string& variable, const float& r, const float& lat, const float& lon, float& dr, float& dlat, float& dlon){
 
-	long variable_id = modelReader->getVariableID(variable);
-	return interpolate(variable_id, r, lat, lon, dr, dlat, dlon);
-}
+		long variable_id = modelReader->getVariableID(variable);
+		return interpolate(variable_id, r, lat, lon, dr, dlat, dlon);
+	}
 
-float SWMFInterpolator::interpolate(const long& variable_id, const float& r, const float& lat, const float& lon){
-
-	//r   = r
-	//lat = theta (up to down)
-	//lon = phi (left to right)
-
-
-	float dc0, dc1, dc2;
-	return interpolate(variableID, r, lat, lon, dc0, dc1, dc2);
-
-	//here I am taking a point and estimating its value linearly using the
-	//confirmed values of the four points existing at its bottom left, top left, top right, and bottom right, in a square
-	const std::vector<float> * data = modelReader->getVariableByID(variable_id);
-
-	float missingvalue;
-
-	/*
-	So for each point I will have a co-latitude and a longitude. The radius I can assume is some constant.
-	Then, the value that I need will come from basically any another variable, for example e-flux.
-
-	Point 1: the point with the closest phi value that is less than LON
-	Point 2: the point with the close phi value that is greater than LON
-	Point 3: the point with the closest theta value that is less than LAT
-	Point 4: the point with the close theta value that is greater than LAT
-	*/
-
-	float v1,t1,p1;
-	float v2,t2,p2;
-	float v3,t3,p3;
-	float v4,t4,p4;
-
-
-	float * pt1 = {3,4,15};
-	float * pt2 = {9,7,25};
-	float * pt3 = {23,14,5};
-	float * pt4 = {7,24,25};
-
-
-
-
-	14 (r,theta,phi)  15
-
-	   *
-
-			               7
- 4
+	/**
+	 * @param r This parameter is unused, since the grid is 2D.
+	 * @param lat The latitude component of the position.
+	 * @param lon The longitude component of the position.
 	 */
+	float SWMFIonoInterpolator::interpolate(const long& variableID, const float& r, const float& lat, const float& lon){
 
-	return missingvalue;
+		//r   = r
+		//lat = theta (up to down)
+		//lon = phi (left to right)
 
-}
-float interpolate(const long& variable_id, const float& r, const float& lat, const float& lon, float& dr, float& dlat, float& dlon){
 
-}
+		float dc0, dc1, dc2;
+		return interpolate(variableID, r, lat, lon, dc0, dc1, dc2);
+
+
+
+	}
+
+	float SWMFIonoInterpolator::interpolate(const long& variableID, const float& r, const float& lat, const float& lon, float& dr, float& dlat, float& dlon){
+		//here I am taking a point and estimating its value linearly using the
+		//confirmed values of the four points existing at its bottom left, top left, top right, and bottom right, in a square
+
+
+		//so... assume the longitude is in magnetic local time, since that's what they want!
+		float longitude = fmod((lon * 15. + 180.), 360.);
+		const std::vector<float> * data = modelReader->getVariable(variableID);
+//		std::cerr << "variable id: " << variableID << std::endl;
+		/*
+		So for each point I will have a co-latitude and a longitude. The radius I can assume is some constant.
+		Then, the value that I need will come from basically any another variable, for example e-flux.
+
+		Point 0: (psi <= lon, theta <= lat)
+		Point 1: (psi >= lon, theta <= lat)
+		Point 2: (psi >= lon, theta >= lat)
+		Point 3: (psi <= lon, theta >= lat)
+
+		       <--
+		    3--------2
+		    |        |^
+		    |        ||
+		    0--------1
+		       -->
+
+		*/
+
+
+		//psi_size
+		//theta_size
+
+		//to find a position, find the longitude first, then the latitude
+		//longitude is steps of psi_size
+
+		int ilon = Utils<float>::binary_search(*psi_data, 0, (*psi_data).size() - 1, longitude);
+		int ilat = Utils<float>::binary_search(*th_data, 0, (*th_data).size() - 1, lat);
+
+		//the longitude and colatitude were not found!
+		if (ilon < 0 || ilat < 0)
+		{
+			return this->modelReader->getMissingValue();
+		}
+
+//		std::cout << "ilon: " << ilon << std::endl;
+//		std::cout << "ilat: " << ilat << std::endl;
+		//p0: ilon, ilat
+		//p1: ilon+1, ilat
+		//p2: ilon+1, ilat+1
+		//p3: ilon, ilat+1
+//		std::cerr << "lon: " << lon << " ilon: " << ilon << " value: " << (*psi_data)[ilon] << std::endl;
+//		std::cerr << "lat: " << lat << " ilat: " << ilat << " value: " << (*th_data)[ilat] << std::endl;
+		float lon1 = (*psi_data)[ilon];
+		float lon2 = (*psi_data)[ilon+1];
+
+		float lat1 = (*th_data)[ilat];
+		float lat2 = (*th_data)[ilat+1];
+
+		float lonP = (longitude - lon1)/(lon2 - lon1); //the percentage along the ray between lon1 and lon2
+		float latP = (lat - lat1)/(lat2 - lat1);
+
+
+		const std::vector<float> * vData = modelReader->getVariableFromMap(variableID);
+//		if (vData == NULL)
+//			std::cout << "data map was null!!!!" << std::endl;
+		int index0 = index_2D_to_1D(ilat, ilon);
+		int index1 = index_2D_to_1D(ilat, ilon+1);
+		int index2 = index_2D_to_1D(ilat+1, ilon+1);
+		int index3 = index_2D_to_1D(ilat+1, ilon);
+		float d0 = (*vData)[index0];
+		float d1 = (*vData)[index1];
+		float d2 = (*vData)[index2];
+		float d3 = (*vData)[index3];
+
+//		for (int i = 0; i < 10; i++)
+//		{
+//			std::cout << "v[" << i << "]: " << (*vData)[i] << std::endl;
+//		}
+
+//		std::cout << "index0: " << index0 << " index1: " << index1 << " index2: " << index2 << " index3: " << index3 << std::endl;
+//		std::cout << "d0: " << d0 << " d1: " << d1 << " d2: " << d2 << " d3: " << d3 << std::endl;
+//		std::cout << "lonP: " << lonP << " latP: " << latP << std::endl;
+//		std::cout << "lon: " << longitude << " lon1: " << lon1 << " lon2: " << lon2 << std::endl;
+		float p01 = d0*(1.0-lonP) + d1*(lonP);
+		float p32 = d3*(1.0-lonP) + d2*(lonP);
+
+		float value = p01*(1.0-latP) + p32*(latP);
+
+
+		return value;
+	}
+
+	int SWMFIonoInterpolator::index_2D_to_1D(const int& ilat, const int& ilon)
+	{
+		int index = ilon*this->th_data->size() + ilat;
+		return index;
+	}
+
+	SWMFIonoInterpolator::~SWMFIonoInterpolator()
+	{
+
+	}
+
+
+
+
 
 
 
--- kameleon-plus/src/ccmc/SWMFIonoInterpolator.h
+++ kameleon-plus/src/ccmc/SWMFIonoInterpolator.h
@@ -1,43 +1,44 @@
-//
-//  SWMFInterpolator.h
-//  
-//
-//  Created by Nitesh Donti on 1/11/12.
-//  Copyright 2012 Cornell University. All rights reserved.
-//
+/*
+ * SWMFIonoInterpolator.h
+ *
+ *  Created on: May 25, 2012
+ *      Author: David Berrios
+ */
 
-#ifndef _SWMFInterpolator_h
-#define _SWMFInterpolator_h
+#ifndef _SWMFIonoInterpolator_h
+#define _SWMFIonoInterpolator_h
 
 #include "Interpolator.h"
 #include "Model.h"
 
 namespace ccmc{ 
     
-    class SWMFInterpolator: public Interpolator
+    class SWMFIonoInterpolator: public Interpolator
     {
      
     public:
-        SWMFInterpolator(Model * model);
+        SWMFIonoInterpolator(Model * model);
         float interpolate(const std::string& variable, const float& r, const float& lat, const float& lon);
         float interpolate(const std::string& variable, const float& r, const float& lat, const float& lon, float& dr, float& dlat, float& dlon);
         float interpolate(const long& variable_id, const float& r, const float& lat, const float& lon);
         float interpolate(const long& variable_id, const float& r, const float& lat, const float& lon, float& dr, float& dlat, float& dlon);
-        virtual ~SWMFInterpolator();
+        virtual ~SWMFIonoInterpolator();
     
     private:
         
-        std::string r_string;
         std::string th_string;
-        std::string phi_string;
+        std::string psi_string;
+        int theta_size;
+        int psi_size;
 
-        const std::vector<float> * r_data;
-    	const std::vector<float> * th_data;
-       	const std::vector<float> * phi_data;
+        const std::vector<float> * th_data;
+    	const std::vector<float> * psi_data;
+       	//const std::vector<float> * phi_data;
 
-		float previous_r;
 		float previous_th;
-		float previous_phi;
+		float previous_psi;
+
+		int index_2D_to_1D(const int& ilat, const int& ilon);
 
     };
     
