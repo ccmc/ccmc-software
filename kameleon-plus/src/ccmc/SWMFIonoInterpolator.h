2cdb8434298fda9d727d55387c9dfd943d2cf261
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2012-08-01 18:04:06 +0000

    Code cleanup
    git-svn-id: https://ccmc-software.googlecode.com/svn@1515 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/SWMFIonoInterpolator.h
+++ kameleon-plus/src/ccmc/SWMFIonoInterpolator.h
@@ -27,16 +27,18 @@ namespace ccmc{
     private:
         
         std::string th_string;
-        std::string psi_string;
+        std::string phi_string;
         int theta_size;
-        int psi_size;
+        int phi_size;
 
         const std::vector<float> * th_data;
-    	const std::vector<float> * psi_data;
+    	const std::vector<float> * phi_data;
        	//const std::vector<float> * phi_data;
 
 		float previous_th;
-		float previous_psi;
+		float previous_phi;
+		int previous_th_index;
+		int previous_phi_index;
 
 		int index_2D_to_1D(const int& ilat, const int& ilon);
 
