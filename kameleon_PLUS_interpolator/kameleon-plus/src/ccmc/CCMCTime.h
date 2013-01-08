97e656ccd0dea9b8b38c6a401dac0a0a71eaac2b
dberrios <dberrios@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2013-01-08 20:40:15 +0000

    Update for time interpolator
    git-svn-id: https://ccmc-software.googlecode.com/svn@793 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon_PLUS_interpolator/kameleon-plus/src/ccmc/CCMCTime.h
+++ kameleon_PLUS_interpolator/kameleon-plus/src/ccmc/CCMCTime.h
@@ -10,18 +10,48 @@ namespace ccmc
 	class Time
 	{
 		public:
-			int year;
-			int month;
-			int day;
-			int hour;
-			int minute;
-			float second;
 
-			friend std::ostream& operator<<(std::ostream& out, const Time time);
+			Time(const std::string& epochString);
+			Time(double epoch);
+			Time();
 
+			short getDay() const;
+			void setDay(short day);
+			void setEpoch(double epoch);
+			short getHour() const;
+			void setHour(short hour);
+			short getMilliseconds() const;
+			void setMilliseconds(short milliseconds);
+			short getMinute() const;
+			void setMinute(short minute);
+			short getMonth() const;
+			void setMonth(short month);
+			short getSeconds() const;
+			void setSeconds(double seconds);
+			short getYear() const;
+			void setYear(short year);
 			std::string toString() const;
+			double getEpoch() const; //we use CDF_EPOCH as the underlying data structure
+
+		private:
+			double epoch;
+			short year;
+			short month;
+			short day;
+			short hour;
+			short minute;
+			short seconds;
+			short milliseconds;
+			void updateEpoch();
 
 	};
+
+	std::ostream& operator<<(std::ostream& out, const Time time);
+	double operator-(const Time& time1, const Time& time2);
+	bool operator==(const Time& time1, const Time& time2);
+	bool operator<(const Time& time1, const Time& time2);
+	bool operator>(const Time& time1, const Time& time2);
+	bool operator>=(const Time& time1, const Time& time2);
 }
 
 #endif
