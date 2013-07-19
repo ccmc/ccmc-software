fbbdebd7ca38f0e1dee1e091d672a9dcce7fd38a
apembroke@gmail.com <apembroke@gmail.com@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2013-07-19 19:08:40 +0000

    If this is LFM, derive pressure from sounds speed,density
    git-svn-id: https://ccmc-software.googlecode.com/svn@1668 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/KameleonInterpolator_compute_p.cpp
+++ kameleon-plus/src/ccmc/KameleonInterpolator_compute_p.cpp
@@ -5,6 +5,7 @@
 namespace ccmc
 {
 	using namespace ccmc::strings::variables;
+	using namespace ccmc::strings::models;
 
 	/**
 	 * @param variable
@@ -24,21 +25,35 @@ namespace ccmc
 
 		//vector<string> requiredVariables = listOfRequiredVariablesForComponents["p"];
 		float p;
-		if (modelReader->doesVariableExist(p_))
-		{
-			p = interpolateSimple(p_, positionComponent1, positionComponent2, positionComponent3, dComponent1,
-					dComponent2, dComponent3);
-		} else
-		{
-			float n = interpolate(n_, positionComponent1, positionComponent2, positionComponent3, dComponent1,
-					dComponent2, dComponent3);
-			float t = interpolate("T", positionComponent1, positionComponent2, positionComponent3, dComponent1,
-					dComponent2, dComponent3);
-			p = n * t;
 
+		if (modelName == lfm_)
+		{
+			float density = interpolateSimple("rho", positionComponent1, positionComponent2, positionComponent3, dComponent1,
+					dComponent2, dComponent3); //[gm/cc]
+			float soundSpeed = interpolateSimple("V_th", positionComponent1, positionComponent2, positionComponent3, dComponent1,
+					dComponent2, dComponent3); //[cm/s]
+			float gamma = 5.0/3; //adiabatic index
+			// pressure[dyn/cm^2] = density[gm/cc]*(ionSoundSpeed[cm/s])^2/gamma
+			// conversion factor of 1e8: cgs units of pressure are [dyn/cm^2] = .1 [Pa]
+			p = .1*(density)*pow(soundSpeed,2)/gamma; // [Pa]
+			return p;
 		}
+		else{
+			if (modelReader->doesVariableExist(p_))
+			{
+				p = interpolateSimple(p_, positionComponent1, positionComponent2, positionComponent3, dComponent1,
+						dComponent2, dComponent3);
+			} else
+			{
+				float n = interpolate(n_, positionComponent1, positionComponent2, positionComponent3, dComponent1,
+						dComponent2, dComponent3);
+				float t = interpolate("T", positionComponent1, positionComponent2, positionComponent3, dComponent1,
+						dComponent2, dComponent3);
+				p = n * t;
 
-		return p;
+			}
+			return p;
+		}
 
 	}
 
