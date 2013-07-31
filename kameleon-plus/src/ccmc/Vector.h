010c476dfb8a8db0ab3cdd572583cff2108627bf
apembroke@gmail.com <apembroke@gmail.com@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2013-07-31 23:10:28 +0000

    added several new vector operators and functions used by Polyhedron.h
    git-svn-id: https://ccmc-software.googlecode.com/svn@1673 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/Vector.h
+++ kameleon-plus/src/ccmc/Vector.h
@@ -25,10 +25,27 @@ namespace ccmc
 			Vector();
 			Vector(T c0, T c1, T c2);
 			T length();
+			Vector<T> operator+(const Vector<T>& v) const;
+			Vector<T> operator-(const Vector<T>& v) const;
+			Vector<T> operator*(const T& value) const;
+			Vector<T> operator/(const T& value) const;
+			void operator+=(const Vector<T>& v);
+			void operator-=(const Vector<T>& v);
+			void operator*=(const T& value);
+			void operator/=(const T& value);
 			static T dot(Vector<T>& a, Vector<T>& b);
 			static T dot(Vector<T>* a, Vector<T>* b);
 			static Vector<T> cross(Vector<T>& a, Vector<T>& b);
 			static Vector<T> cross(Vector<T>* a, Vector<T>* b);
+			static void cross(Vector<T>& c, Vector<T>& a, Vector<T>& b);
+			static T triple(Vector<T>& a,Vector<T>& b,Vector<T>& c);
+			static Vector<T> norm(Vector<T>& a);
+			static Vector<T> norm(Vector<T>* a);
+			void norm();
+			static T angle(Vector<T>& a, Vector<T>& b);
+			static void angle(T& angle, Vector<T>& a, Vector<T>& b);
+
+			static Vector<T> dummy;
 
 			virtual ~Vector();
 	};
@@ -39,6 +56,9 @@ namespace ccmc
 	{
 
 	}
+
+	template<class T>
+	Vector<T> Vector<T>::dummy = Vector(0,0,0);
 
 	template<class T>
 	Vector<T>::~Vector()
@@ -86,6 +106,114 @@ namespace ccmc
 	{
 		return Vector<T> (a->c1() * b->c2() - a->c2() * b->c1(), a->c2() * b->c0() - a->c0() * b->c2(), a->c0()
 				* b->c1() - a->c1() * b->c0());
+	}
+
+	template<class T>
+	void Vector<T>::cross(Vector<T>& c, Vector<T> &a, Vector<T> &b) //should be faster than cross(a,b)
+	{ // c = a cross b
+		c.components[0] = a.c1() * b.c2() - a.c2() * b.c1();
+		c.components[1] = a.c2() * b.c0() - a.c0() * b.c2();
+		c.components[2] = a.c0() * b.c1() - a.c1() * b.c0();
+	}
+
+	template<class T>
+	T Vector<T>::triple(Vector<T>& a,Vector<T>& b,Vector<T>& c){
+		Vector<T> temp = cross(b,c);
+		return dot(a,temp);
+	}
+
+	template<class T>
+	T Vector<T>::angle(Vector<T>& a, Vector<T>& b)
+	{
+		return T(atan2(Vector<T>::cross(a,b).length(),Vector<T>::dot(a,b)));
+	}
+
+	template<class T>
+	void Vector<T>::angle(T& angle, Vector<T>& a, Vector<T>& b) //should be faster than angle(a,b)
+	{
+		Vector<T>::cross(Vector<T>::dummy, a, b); //dummy stores cross product of a and b
+		angle = atan2(Vector<T>::dummy.length(), Vector<T>::dot(a,b));
+		return;
+	}
+
+	template<class T>
+	Vector<T> Vector<T>::norm(Vector<T>& a)
+	{
+		T length = a.length();
+		return Vector<T> (a.c0() / length, a.c1() / length, a.c2()/length);
+	}
+
+	template<class T>
+	Vector<T> Vector<T>::norm(Vector<T>* a)
+	{
+		T length = a->length();
+		return Vector<T> (a->c0() / length, a->c1() / length, a->c2()/length);
+	}
+
+	template<class T>
+	void Vector<T>::norm()
+	{
+		//call the overloaded /= operator and normalize the components of this object
+		(*this)/=this->length();
+	}
+
+	template<class T>
+	Vector<T> Vector<T>::operator+(const Vector<T>& v) const
+	{
+		return Vector<T> (this->components[0]+v.components[0],this->components[1]+v.components[1],
+				this->components[2]+v.components[2]);
+	}
+
+	template<class T>
+	void Vector<T>::operator+=(const Vector<T>& v)
+	{
+		this->components[0]+=v.components[0];
+		this->components[1]+=v.components[1];
+		this->components[2]+=v.components[2];
+	}
+
+	template<class T>
+	Vector<T> Vector<T>::operator-(const Vector<T>& v) const
+	{
+		return Vector<T> (this->components[0]-v.components[0],this->components[1]-v.components[1],
+				this->components[2]-v.components[2]);
+	}
+
+	template<class T>
+	void Vector<T>::operator-=(const Vector<T>& v)
+	{
+		this->components[0]-=v.components[0];
+		this->components[1]-=v.components[1];
+		this->components[2]-=v.components[2];
+	}
+
+	template<class T>
+	Vector<T> Vector<T>::operator*(const T& value) const
+	{
+		return Vector<T> (this->components[0]*value, this->components[1]*value,this->components[2]*value);
+
+	}
+
+	template<class T>
+	void Vector<T>::operator*=(const T& value)
+	{
+		this->components[0]*=value;
+		this->components[1]*=value;
+		this->components[2]*=value;
+	}
+
+	template<class T>
+	Vector<T> Vector<T>::operator/(const T& value) const
+	{
+		return Vector<T> (this->components[0]/value, this->components[1]/value, this->components[2]/value);
+	}
+
+	template<class T>
+	void Vector<T>::operator/=(const T& value)
+	{
+		this->components[0]/=value;
+		this->components[1]/=value;
+		this->components[2]/=value;
 	}
 
 }
