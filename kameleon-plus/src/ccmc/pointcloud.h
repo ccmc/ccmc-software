c60474c3878dbfd298eded475e5108fd80e87ff4
apembroke@gmail.com <apembroke@gmail.com@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2013-08-24 00:27:03 +0000

    fixed nanoflann reference - should compile on both mac and linux now
    git-svn-id: https://ccmc-software.googlecode.com/svn@1690 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon-plus/src/ccmc/pointcloud.h
+++ kameleon-plus/src/ccmc/pointcloud.h
@@ -26,7 +26,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *************************************************************************/
 
-#include <ccmc/nanoflann.hpp>
+#include "nanoflann.hpp"
 #include <cstdlib>
 #include <iostream>
 
