10715cf5f8fa007a853ba932aea819652f2d8159
marlo.maddox@gmail.com <marlo.maddox@gmail.com@acdf4c94-f73e-48c1-5845-2db45fb2849b>
2013-09-17 17:30:53 +0000

    removing superfluous dir
    git-svn-id: https://ccmc-software.googlecode.com/svn@1698 acdf4c94-f73e-48c1-5845-2db45fb2849b

--- kameleon_converter/branches/kameleon_converter-UMICH-DEV/kameleon-converter-working1/inc/ccmc_missing.h
+++
@@ -1,9 +0,0 @@
-/* CCMC MISSING - define macros to identify missing data */
-#ifndef CCMC_MISSING_H_
-#define CCMC_MISSING_H_
-
-#define FLOAT_MASK     -1.09951162778e12    /** -256^5 **/
-#define INT_MASK     -2147483647            /** ( -2^31 ) - 1 **/
-
-#endif
-
